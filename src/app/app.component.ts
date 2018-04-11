f517d56141ec1dede679304477b94de51de17c51
Pavel Vernigorov <pavelvernigorov@gmail.com>
2018-04-11 19:27:26 +0300

    HttpClient configured

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  apiRoot : string = 'http://localhost:4020';
+
+  constructor(private httpClient : HttpClient) {}
+
+  getArticles() {
+    let url = `${ this.apiRoot }/api/articles/cv4lg3/list`;
+
+    return this.httpClient.get(url)
+      .subscribe(
+        data => {
+          console.log(data);
+        }
+      );
+  }
+}
